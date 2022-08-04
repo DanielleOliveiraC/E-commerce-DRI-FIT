5cb8edd4edb88499c0dbd3cea951eff07909dce9
Danielle de Oliveira <78177394+DanielleOliveiraC@users.noreply.github.com>
2022-08-04 09:58:05 -0300

    Add files via upload

---
+++ Site/Fale Conosco.php
@@ -0,0 +1,391 @@
+﻿<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>DRI-FIT</title>
+<link href="CSS/Fale conosco.css" rel="stylesheet" type="text/css">
+</head>
+<body link="#FFFFFF" vlink="#FFFFFF" alink="#FFFFFF">
+<link rel="stylesheet" href="css/reset.css">
+
+<!--FONTES-->
+<link href="https://fonts.googleapis.com/css?family=Red+Hat+Display&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+<link href = "https://fonts.googleapis.com/CSS? família =Merriweather& display = swap" rel = "stylesheet"> 
+<div id="header">
+
+<div class="logo"><a href="Index.html"><img src="img/logotipo.jpg"></a></div>
+
+<form class="searchbox" method="get" action="/search" autocomplete="off">
+<input name="q" type="text" size="15" placeholder="Digite aqui sua pesquisa..." />
+<input class="button-submit" type="submit" value=""/></form>
+    
+<div class="ECbotão"><a href="Login.html"><img src="img/login.jpg"></a></div>
+<div class="EC"><a href="Login.html"><b><p1>Entre</p1></b></a> <p1>ou</p1> <a href="Cadastro.php"><b><p1>Cadastre-se</p1></b></a></div>
+
+<div class="carrinho"><a href="Pedidos.html"><img src="img/carrinho.jpg"></a></div></div>
+
+
+<div id="mainnav">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<div class="menu">
+<a href="Ofertas.html"><p9>Ofertas</p9></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="Cartão Presente.html"><p9>Cartão presente</p9></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="APP.html"><p9>Baixe o APP</p9></a>
+</div>
+
+<div class="cd-dropdown-wrapper">
+<a class="cd-dropdown-trigger" href="#0"><p10>Todas as categorias</10></a>
+<nav class="cd-dropdown">
+<a href="#0" class="cd-close">Close</a>
+<ul class="cd-dropdown-content">
+					
+<li class="has-children">
+<a href="#"><p9>Masculino</9></a>
+<ul class="cd-secondary-dropdown is-hidden">
+<li class="go-back"><a href="#0">Menu</a></li>
+
+<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Roupas</a>
+	<ul class="is-hidden">
+			<li><a href="BermudasM.html"><c><p10>Bermudas</p10></c></a></li>
+		<li><a href="CalçasM.html"><c><p10>Calças</p10></c></a></li>
+		<li><a href="CamisetasM.html"><c><p10>Camisetas</p10></c></a></li>
+		<li><a href="MoletonsM.html"><c><p10>Moletons</p10></c></a></li>
+		</ul>
+		</li>
+	<li class="has-children">
+			<div class="imgmenu1"><img src="img/imgmenuM1.jpg"></div>
+    		<div class="imgmenu2"><img src="img/imgmenuM2.jpg"></div>
+		</li>
+	<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Calçados</a>
+	<ul class="is-hidden">
+		<li><a href="ChinelosM.html"><c><p10>Chinelos</p10></c></a></li>
+		<li><a href="ChuteirasM.html"><c><p10>Chuteiras</p10></c></a></li>
+		<li><a href="SapatênisM.html"><c><p10>Sapatênis</p10></c></a></li>
+		<li><a href="TênisM.html"><c><p10>Tênis</p10></c></a></li>
+		</ul>
+		</li>
+		</ul> 
+		</li> 
+
+<li class="has-children">
+<a href="#"><p9>Feminino</p9></a>
+<ul class="cd-secondary-dropdown is-hidden">
+<li class="go-back"><a href="#0">Menu</a></li>
+<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Roupas</a>
+    <ul class="is-hidden">
+		<li><a href="CalçasF.html"><c><p10>Calças</p10></c></a></li>
+		<li><a href="CamisetasF.html"><c><p10>Camisetas</p10></c></a></li>
+		<li><a href="MoletonsF.html"><c><p10>Moletons</p10></c></a></li>
+		<li><a href="ShortsF.html"><c><p10>Shorts</p10></c></a></li>
+		</ul>
+		</li>
+		<li class="has-children">
+			<div class="imgmenu1"><img src="img/imgmenuF1.jpg"></div>
+    		<div class="imgmenu2"><img src="img/imgmenuF2.jpg"></div>
+		</li>
+		<li class="has-children">
+		<a href="http://codyhouse.co/?p=748">Calçados</a>
+		<ul class="is-hidden">
+			<li><a href="BotasF.html"><c><p10>Botas</p10></c></a></li>
+			<li><a href="ChinelosF.html"><c><p10>Chinelos</p10></c></a></li>
+			<li><a href="SapatilhasF.html"><c><p10>Sapatilhas</p10></c></a></li>
+			<li><a href="TênisF.html"><c><p10>Tênis</p10></c></a></li>
+			</ul>
+			</li>
+        </ul> 
+        </li> 
+        
+<li class="has-children">
+<a href="#"><p9>Infantil</9></a>
+<ul class="cd-secondary-dropdown is-hidden">
+<li class="go-back"><a href="#0">Menu</a></li>
+<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Roupas</a>
+	<ul class="is-hidden">
+		<li><a href="CalçasI.html"><c><p10>Calças</p10></c></a></li>
+		<li><a href="CamisetasI.html"><c><p10>Camisetas</p10></c></a></li>
+		<li><a href="MoletonsI.html"><c><p10>Moletons</p10></c></a></li>
+		<li><a href="ShortsI.html"><c><p10>Shorts</p10></c></a></li>
+		</ul>
+		</li>
+	<li class="has-children">
+		<div class="imgmenu1"><img src="img/imgmenuI1.jpg"></div>
+    	<div class="imgmenu2"><img src="img/imgmenuI2.jpg"></div>
+		</li>
+	<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Calçados</a>
+	<ul class="is-hidden">
+		<li><a href="ChinelosI.html"><c><p10>Chinelos</p10></c></a></li>
+		<li><a href="ChuteirasI.html"><c><p10>Chuteiras</p10></c></a></li>
+		<li><a href="SapatilhasI.html"><c><p10>Sandálias e Sapatilhas</p10></c></a></li>
+		<li><a href="TênisI.html"><c><p10>Tênis</p10></c></a></li>
+		</ul>
+		</li>
+		</ul> 
+		</li> 
+
+<li class="has-children">
+<a href="#"><p9>Acessórios</9></a>
+<ul class="cd-secondary-dropdown is-hidden">
+<li class="go-back"><a href="#0">Menu</a></li>
+<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Masculino</a>
+	<ul class="is-hidden">
+		<li><a href="BolasM.html"><c><p10>Bolas</p10></c></a></li>
+		<li><a href="MeiõesM.html"><c><p10>Meiões</p10></c></a></li>
+		<li><a href="MochilasM.html"><c><p10>Mochilas</p10></c></a></li>
+		<li><a href="RelógiosM.html"><c><p10>Relógios</p10></c></a></li>
+		</ul>
+		</li>
+	<li class="has-children">
+		<div class="imgmenu1"><img src="img/imgmenuA1.jpg"></div>
+    	<div class="imgmenu2"><img src="img/imgmenuA2.jpg"></div>
+		</li>
+	<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Feminino</a>
+	<ul class="is-hidden">
+		<li><a href="BolsasF.html"><c><p10>Bolsas</p10></c></a></li>
+		<li><a href="MochilasF.html"><c><p10>Mochilas</p10></c></a></li>
+		<li><a href="RelógiosF.html"><c><p10>Relógios</p10></c></a></li>
+		<li><a href="LuvasF.html"><c><p10>Luvas</p10></c></a></li>
+		</ul>
+		</li>
+		</ul> 
+		</li>
+        
+<li class="has-children">
+<a href="#"><p9>Suplementos</9></a>
+<ul class="cd-secondary-dropdown is-hidden">
+<li class="go-back"><a href="#0">Menu</a></li>
+<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Alimentação</a>
+	<ul class="is-hidden">
+		<li><a href="Adoçante e açúcares.html"><c><p10>Adoçante e açúcares</p10></c></a></li>
+		<li><a href="Cereais, grãos e frutas.html"><c><p10>Cereais, grãos e frutas</p10></c></a></li>
+		<li><a href="Snacks e barras.html"><c><p10>Snacks e barras</p10></c></a></li>
+		</ul>
+		</li>
+	<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Ganho de massa</a>
+	<ul class="is-hidden">
+		<li><a href="Barras protéicas.html"><c><p10>Barras protéicas</p10></c></a></li>
+		<li><a href="Proteínas.html"><c><p10>Proteínas</p10></c></a></li>
+		<li><a href="Whey Protein.html"><c><p10>Whey Protein</p10></c></a></li>
+		</ul>
+		</li>
+	<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Pré treino</a>
+	<ul class="is-hidden">
+		<li><a href="Cafeína.html"><c><p10>Cafeína</p10></a></c></li>
+		<li><a href="Refeição e Shakes.html"><c><p10>Refeição e Shakes</p10></c></a></li>
+		<li><a href="Termogênicos.html"><c><p10>Termogênicos</p10></c></a></li>
+		</ul>
+		</li>
+	<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Vitaminas</a>
+    <ul class="is-hidden">
+		<li><a href="Colágenos.html"><c><p10>Colágenos</p10></a></c></li>
+		<li><a href="Funcionais.html"><c><p10>Funcionais - Fitoterápicos</p10></c></a></li>
+		<li><a href="Multivitamínicos.html"><c><p10>Multivitamínicos</p10></c></a></li>
+		</ul>
+		</li>
+		</ul> 
+		</li> 
+        
+<li class="has-children">
+<a href="#"><p9>Outros</9></a>
+<ul class="cd-secondary-dropdown is-hidden">
+<li class="go-back"><a href="#0">Menu</a></li>
+<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Equipamentos</a>
+	<ul class="is-hidden">
+		<li><a href="Bolas.html"><c><p10>Bolas</p10></c></a></li>
+		<li><a href="Para Jogar.html"><c><p10>Para Jogar</p10></c></a></li>
+		<li><a href="Para se Mover.html"><c><p10>Para se Mover</p10></c></a></li>
+		<li><a href="Para se Proteger.html"><c><p10>Para se Proteger</p10></c></a></li>
+		</ul>
+		</li>
+	<li class="has-children">
+		<div class="imgmenu1"><img src="img/imgmenuO1.jpg"></div>
+   		<div class="imgmenu2"><img src="img/imgmenuO2.jpg"></div>
+		</li>
+	<li class="has-children">
+	<a href="http://codyhouse.co/?p=748">Esportes</a>
+	<ul class="is-hidden">
+		<li><a href="Jogos de mesa.html"><c><p10>Jogos de mesa</p10></c></a></li>
+		<li><a href="Mergulho.html"><c><p10>Mergulho</p10></c></a></li>
+		<li><a href="Patins.html"><c><p10>Patins</p10></c></a></li>
+		<li><a href="Pilates e Yoga.html"><c><p10>Pilates e Yoga</p10></c></a></li>
+		</ul>
+		</li>
+		</ul> 
+		</li> 
+<script src="js/jquery-2.1.1.js"></script><!--faz abrir o menu-->
+<script src="js/jquery.menu-aim.js"></script><!--faz abrir as categorias-->
+<script src="js/main.js"></script><!--faz abrir o menu--> 
+</div>
+
+<div id="contents">
+
+<div class="portal"><img src="img/Fale conosco.jpg"></div>
+
+<div class="info">
+<p13><center>Envio e Entrega</center></p13><br>
+<a href="Envio e Entrega.html"><c><p19>Como é feita a entrega</p19></c></a><br>
+<a href="Envio e Entrega.html"><c><p19>Prazo de entrega</p19></c></a><br>
+<a href="Envio e Entrega.html"><c><p19>Serviço de montagem</p19></c></a>
+</div>
+
+
+<div class="info2">
+<p13><center>Política de Trocas</center></p13><br>
+<a href="Política de Trocas e Arrependimento.html"><c><p19>Devolução dos produtos</p19></c></a><br>
+<a href="Política de Trocas e Arrependimento.html"><c><p19>Trocas por defeito</p19></c></a><br>
+<a href="Política de Trocas e Arrependimento.html"><c><p19>Trocas por arrependimento</p19></c></a>
+</div>
+
+
+<div class="info2">
+<p13><center>Política de Privacidade</center></p13><br>
+<a href="Política de Privacidade.html"><c><p19>Confidencialidade e segurança</p19></c></a><br>
+<a href="Política de Privacidade.html"><c><p19>Dicas importantes</p19></c></a><br>
+<a href="Política de Privacidade.html"><c><p19>Melhora contínua</p19></c></a>
+</div>
+
+<div class="info2">
+<p13><center>Segurança</center></p13><br>
+<a href="Segurança.html"><c><p19>Segurança de informações</p19></c></a><br>
+<a href="Segurança.html"><c><p19>Privacidade de dados</p19></c></a><br>
+<a href="Segurança.html"><c><p19>Instalação de cookies</p19></c></a>
+</div>
+
+
+		<form method="POST" action="salva_mensagemfc.php">
+<div class="cadastro">
+<p13>Com dúvidas? Nos mande sua pergunta!</p13><br><br><br>
+<label for="Nome"><p10> Nome </p10></label>
+&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+<label for="email"><p10> E-mail </p10></label>
+<div>
+<input type="Nome" name="nome" maxlength="100" size="70" required autocomplete="off"> &nbsp&nbsp&nbsp
+<input type="Email" name="email" maxlength="100" size="81" required autocomplete="off"> <br> <br> </div>
+<label for="Pergunta"><p10> Pergunta </p10></label> 
+<div>
+<input type="Pergunta" name="pergunta" maxlength="300" size="140" required autocomplete="off"> 
+<div class="enviar">
+<div class="submit">
+<br><input type="submit" onClick="Enviar();" value="Enviar">
+</div>
+</div>
+</div>
+</form>
+</div>
+
+<div class="DF"><p22>Dúvidas frequentes</p22></div>
+<div id="menu-home">
+  <div class="menu">
+      <ul class="categorias-content">
+      <li class="categoria">
+              <a class="seta-direita" href="#"><p20>Como é feita a entrega de produtos?</p20></a>
+              <ul class="subcategorias-content" style="display: none;">
+          <li class="subcategoria">
+                      <a href="#"><p19>Todos os produtos vendidos e entregues por DRI-FIT, além da embalagem do fabricante, são enviados em caixas ou envelopes exclusivos, desenvolvidos para garantir a qualidade dos produtos e evitar avarias. Antes de receber a entrega, verifique se o pacote não está violado. Caso esteja, não aceite o pacote e entre em contato com a loja.
+Se notar que o pedido foi violado somente depois de recebê-lo, por favor, entre em contato com a gente em até 72 horas.</p19></a>
+                  </li>
+              </ul>
+          </li>
+      <li class="categoria">
+        <a class="seta-direita" href="#"><p20>Qual é o prazo de entrega do meu pedido?</p20></a>
+        <ul class="subcategorias-content">
+          <li class="subcategoria">
+                  <a href="#"><p19>Após realizar a compra, o prazo de entrega será contado após confirmação dos seus dados cadastrais e confirmação do pagamento, sendo que a análise de dados cadastrais poderá levar até 1 dia útil.<br>
+Algumas transportadoras disponibilizam a comodidade do rastreamento de produtos em seu próprio site.</p19></a>
+              </li>
+        </ul>
+      </li>
+      <li class="categoria">
+        <a class="seta-direita" href="#"><p20>Como devolver os produtos?</p20></a>
+        <ul class="subcategorias-content">
+          <li class="subcategoria">
+                      <a href="#"><p19>Para produtos leves a devolução para troca pelo site é feita por meio de postagem nos Correios. Ao solicitar a troca, te daremos um código de postagem e você deverá comparecer em qualquer agência credenciada dos Correios com o produto pronto para envio.<br>Para produtos Grandes/Pesados fazemos a coleta no endereço de entrega. Mas veja bem: são considerados grandes e pesados os produtos com mais de 30kg e/ou com dimensões acima de 160cm³ (comprimento X altura X largura).</p19></a>
+                  </li>
+        </ul>
+      </li>
+      <li class="categoria">
+        <a class="seta-direita" href="#"><p20>Como funciona as trocas por arrependimento?</p20></a>
+        <ul class="subcategorias-content">
+          <li class="subcategoria">
+              <a href="#"><p19>Queremos sempre ganhar a medalha de ouro quando o assunto é superar as suas expectativas. Mas caso você se arrependa da compra de um produto do nosso site, aplicativo ou televendas, você tem o direito de desistir da compra em até 30 dias corridos da data de entrega (exceto para itens considerados grandes e/ou pesados, acessórios, equipamentos, eletrônicos e suplementos em que o prazo é de 7 dias e produtos personalizados que não têm direito a devolução por arrependimento).</p19></a>
+          </li>
+        </ul>
+      </li>
+      
+      <li class="categoria">
+        <a class="seta-direita" href="#"><p20>Como funciona a confidencialidade de dados?</p20></a>
+        <ul class="subcategorias-content">
+          <li class="subcategoria">
+              <a href="#"><p19>Todas as informações são codificadas (criptografadas), armazenadas com segurança e respeitam a todo instante sua privacidade. Assumimos o compromisso de que, em nenhuma hipótese, divulgaremos os dados cadastrais de nossos clientes.</p19></a>
+          </li>
+        </ul>
+      </li>
+      
+      <li class="categoria">
+        <a class="seta-direita" href="#"><p20>Como funcionam as trocas de cor/modelo/tamanho?</p20></a>
+        <ul class="subcategorias-content">
+          <li class="subcategoria">
+              <a href="#"><p19>Se você comprou e recebeu um produto que não te serviu, ou não era da cor ou modelo que você queria, você tem até 30 dias corridos a partir do recebimento para solicitar a troca (exceto para itens considerados grandes e/ou pesados, acessórios, equipamentos, eletrônicos e suplementos, em que o prazo é de 7 dias, e produtos personalizados, que não têm direito a troca).</p19></a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</div>
+<!-- partial -->
+  <script src='https://code.jquery.com/jquery-2.1.4.min.js'></script><script  src="js/perguntas.js"></script>
+
+
+
+
+
+</div>
+
+
+<div class="caixa"><img src="img/caixa.jpg"></div>
+
+<div id="footer">
+
+<div class="rodapé1">
+<p6>Formas de Pagamento</p6><img src="img/cartão.jpg"><br><br>
+<p6>Certificações</p6><img src="img/certificações.jpg">
+
+<div class="rodapé2">
+<p6>Institucional</p6><br>
+<a href="Sobre.html"><p7><b>Sobre a DRI-FIT</b></p7></a><br>
+<a href="Política de Privacidade.html"><p7><b>Política de Privacidade</b></p7></a>><br>
+<a href="Política de Trocas e Arrependimento.html"><p7><b>Política de Trocas e Arrependimento</b></p7></a><br><br>
+<p6>Ajuda e Suporte</p6><br>
+<a href="Segurança.html"><p7><b>Segurança</b></p7></a><br>
+<a href="Envio e Entrega.html"><p7><b>Envio e Entrega</b></p7></a><br>
+<a href="Fale Conosco.php"><p7><b>Fale Conosco</b></p7></a>
+
+<div class="rodapé3">
+<p6>Cartão Presente</p6><br>
+<a href="Cartão Presente.html"><p7><b>Saiba mais sobre o cartão presente DRI-FIT</b></p7></a><br><br>
+<p6>Permaneça conectado</p6><br>
+<a href="https://www.facebook.com/Dri-Fit-113038433408926/"><div class="facebook"></div></a>
+<a href="https://www.instagram.com/p/B2DX5_Nl9z_/?igshid=hvwvur3yi8kk"><div class="instagram"></div></a>
+<a href="https://twitter.com/drifitoficial"><div class="twitter"></div></a><br><br><br>
+<p6>App DRI-FIT</p6><br>
+<a href="APP.html"><p7><b>Conheça as vantagens</b></p7></a>
+</div></div></div></div>
+
+<div class="rodapé4">
+<center><p8>Os preços, promoções, condições de pagamento, frete e produtos são válidos exclusivamente para compras realizadas via internet. Criado e desenvolvido por Wollner.<br>Fotos meramente ilustrativas. Copyright  © - drifit.com.br. Todos os direitos reservados.</p8></center>
+</div>
+
+</body>
+</html>
